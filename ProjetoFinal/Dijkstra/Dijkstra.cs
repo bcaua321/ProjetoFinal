abd853d2fb25940da820a59b65d0f169fabc4e12
Breno <bcaua205@gmail.com>
2022-11-27 21:38:31 -0300

    Implementando Print do Melhor caminho e alterando visibilidade dos métodos/atributos

--- ProjetoFinal/Dijkstra/Dijkstra.cs
+++ ProjetoFinal/Dijkstra/Dijkstra.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Grafos.Dijkstra
 {
     public class DijkstraAlg
     {
-        private IList<CaminhoInfo> Caminhos { get; } // Arestas contendo os valores das conexões entre os computadores
-        public IList<Caminho> Caminho { get; } // Resultado depois de percorrer todas os computadores 
-        private IList<Computador> ComputadoresNaoVisitados { get; } // Para ter o controle dos computadores não visitados
+        // Arestas contendo os valores das conexões entre os computadores
+        private IList<CaminhoInfo> Caminhos { get; }
+
+        // Resultado depois de percorrer todas os computadores 
+        private IList<Caminho> Caminho { get; }
+
+        // Para ter o controle dos computadores não visitados
+        private IList<Computador> ComputadoresNaoVisitados { get; }
         public DijkstraAlg(List<CaminhoInfo> arestas, List<Computador> computadores, int computadorInicialId)
         {
-            Caminhos = arestas;
+            Caminhos = PreencherGrafoBiDirecional(arestas);
             ComputadoresNaoVisitados = computadores;
             Caminho = new List<Caminho>();
             Preencher(computadorInicialId);
@@ -22,28 +28,29 @@ namespace Grafos.Dijkstra
             if (ComputadoresNaoVisitados.Count == 0)
                 return;
 
-            var min = TakeMin(); // Irá pegar o menor valor que esteja nos vertices não visitados
+            // Irá pegar o menor valor que esteja nos vertices não visitados
+            var min = TakeMin();
 
-            var verticesAdj = VerticesAdjacentes(min.Id); // Depois irá pegar todos os vértices adjacentes ao vertice escolhido
+            // Irá pegar todos os vértices adjacentes ao vertice escolhido acima
+            var verticesAdj = VerticesAdjacentes(min.ComputadorId);
 
-            foreach(var item in verticesAdj)
+            foreach (var item in verticesAdj)
             {
                 var soma = min.Tamanho + item.CustoOp;
-                if(soma < PegarValorVertice(item.ComputadorDois)) 
+                if (soma < PegarValorVertice(item.ComputadorDois))
                 {
-                   AtualizarValores(item.ComputadorDois, item.ComputadorUm, soma);   
+                    AtualizarValores(item.ComputadorDois, item.ComputadorUm, soma);
                 }
             }
 
             Executar();
         }
 
-        public Caminho TakeMin()
+        private Caminho TakeMin()
         {
-            var result = Caminho
-            .FirstOrDefault(x => x.Tamanho < int.MaxValue &&
-            ComputadoresNaoVisitados.Any(v => v.Id == x.Id)); // Pega o primeiro valor menor que o simbolico e que esteja nos vertices não visitados
-            var vertice = ComputadoresNaoVisitados.Where(x => x.Id == result.Id).FirstOrDefault();
+            // Pega o primeiro valor menor que o simbolico e que esteja nos vertices não visitados
+            var result = Caminho.FirstOrDefault(x => x.Tamanho < int.MaxValue && ComputadoresNaoVisitados.Any(v => v.Id == x.ComputadorId));
+            var vertice = ComputadoresNaoVisitados.Where(x => x.Id == result.ComputadorId).FirstOrDefault();
 
             // remove do vertices não visitados
             ComputadoresNaoVisitados.Remove(vertice);
@@ -51,21 +58,21 @@ namespace Grafos.Dijkstra
             return result;
         }
 
-        public List<CaminhoInfo> VerticesAdjacentes(int computadorUm)
+        private List<CaminhoInfo> VerticesAdjacentes(int computadorUm)
         {
             return Caminhos.Where(x => x.ComputadorUm == computadorUm).ToList();
         }
 
         // Retorna o valor atribuido ao vertice
-        public int PegarValorVertice(int n)
+        private int PegarValorVertice(int n)
         {
-            var result = Caminho.FirstOrDefault(x => x.Id == n).Tamanho;
+            var result = Caminho.FirstOrDefault(x => x.ComputadorId == n).Tamanho;
             return result;
         }
 
-        public void AtualizarValores(int vertice, int prev, int valor)
+        private void AtualizarValores(int vertice, int prev, int valor)
         {
-            var result = Caminho.FirstOrDefault(x => x.Id == vertice);
+            var result = Caminho.FirstOrDefault(x => x.ComputadorId == vertice);
 
             result.Tamanho = valor;
             result.Prev = prev;
@@ -89,6 +96,35 @@ namespace Grafos.Dijkstra
                 }
 
                 Caminho.Add(vertice);
+            }
+        }
+
+        // Irá pegar as arestas que foram passadas, criar uma nova com os vertices invertidos, para assim ter um grafo bidirecional
+        private List<CaminhoInfo> PreencherGrafoBiDirecional(List<CaminhoInfo> arestas)
+        {
+            var listResult = new List<CaminhoInfo>();
+            foreach (var item in arestas)
+            {
+                var novaAresta = new CaminhoInfo(item.ComputadorDois, item.ComputadorUm, item.CustoOp); // Inverte os vertices
+                listResult.Add(novaAresta);
+                listResult.Add(item);
+            }
+
+            return listResult;
+        }
+
+        public void PrintCaminho()
+        {
+            Console.WriteLine("Melhor caminho: ");
+            foreach (var item in Caminho.ToList())
+            {
+                if(item.Prev == null)
+                {
+                    Console.WriteLine($"Começo ->  Computador {item.ComputadorId} ");
+                    continue;
+                }
+
+                Console.WriteLine($"| Computador {item.Prev} ->  Computador {item.ComputadorId} |");
             }
         }
     }
