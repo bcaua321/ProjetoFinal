d90abb5311790d753a747d0c9e4b253f983e6839
Breno <bcaua205@gmail.com>
2022-11-27 21:39:09 -0300

    Implementando novos valores

--- ProjetoFinal/Program.cs
+++ ProjetoFinal/Program.cs
@@ -12,35 +12,20 @@ namespace ProjetoFinal
             List<CaminhoInfo> arestas = new List<CaminhoInfo>
             {
                 new CaminhoInfo(1, 2, 3),
-                new CaminhoInfo(2, 1, 3),
                 new CaminhoInfo(1, 3, 10),
-                new CaminhoInfo(3, 1, 10),
                 new CaminhoInfo(3, 2, 6),
-                new CaminhoInfo(2, 3, 6),
                 new CaminhoInfo(3, 4, 9),
-                new CaminhoInfo(4, 3, 9),
                 new CaminhoInfo(2, 4, 4),
-                new CaminhoInfo(4, 2, 4),
                 new CaminhoInfo(2, 5, 6),
                 new CaminhoInfo(5, 2, 6),
-                new CaminhoInfo(2, 5, 6),
                 new CaminhoInfo(3, 5, 5),
-                new CaminhoInfo(5, 3, 5),
-                new CaminhoInfo(4, 2, 4),
                 new CaminhoInfo(4, 2, 4),
                 new CaminhoInfo(4, 5, 3),
-                new CaminhoInfo(5, 4, 3),
                 new CaminhoInfo(4, 6, 9),
-                new CaminhoInfo(6, 4, 9),
                 new CaminhoInfo(4, 7, 9),
-                new CaminhoInfo(7, 4, 9),
                 new CaminhoInfo(5, 6, 4),
-                new CaminhoInfo(6, 5, 4),
                 new CaminhoInfo(5, 7, 5),
-                new CaminhoInfo(7, 5, 5),
-                new CaminhoInfo(7, 6, 5),
-                new CaminhoInfo(6, 7, 5),
-
+                new CaminhoInfo(7, 6, 5)
             };
 
 
@@ -58,8 +43,7 @@ namespace ProjetoFinal
 
             DijkstraAlg dik = new DijkstraAlg(arestas, vertices, 1);
             dik.Executar();
-            var result = dik.Caminho;
-
+            dik.PrintCaminho();
         }
     }
 }
